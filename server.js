5251aaa43e79181bce41ea6a91bc409814f5809b
rodo08 <rodrigo.rosalesmoya@gmail.com>
2025-02-16 19:34:24 +0100

    Add netlify to allowed origins

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
 
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://mern-signinup-otp.netlify.app",
+];
 
 app.use(express.json());
 app.use(cookieParser());
